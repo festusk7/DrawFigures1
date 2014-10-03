b47c57a09e82a9d6066b8184cfb642a63b0f7773
festusk7 <festus.konneh@techhigh.us>
2014-10-03 09:52:30 -0400

    Added.java

--- time.java
+++ time.java
@@ -2,12 +2,10 @@ public class time {
 public static void main(String args[]) {
 int sec = 5000;
 int hours = sec / 3600;
-int remainder = sec % 3600;
-int minutes = remainder / 60;
- int secs = remainder % 60;
-System.out.println("Hours in 5000 seconds = " + hours);
-System.out.println("Minutes in 5000  seconds = " + minutes);
-System.out.println("Seconds in 5000 seconds = " + secs);
+int minutes = (sec % 3600) / 60;
+ int secs = (sec % 3600) % 60;
+System.out.println(+ sec + " seconds = " + hours + " hours, " + minutes + " minutes, " + secs + " seconds");
+
 }
 }
 
