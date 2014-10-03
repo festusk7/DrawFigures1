a39dc4d021d36f4280105cd1577370b0a52ed428
festusk7 <festus.konneh@techhigh.us>
2014-10-03 09:58:59 -0400

    Added.java

--- time.java
+++ time.java
@@ -4,7 +4,7 @@ int sec = 5000;
 int hours = sec / 3600;
 int minutes = (sec % 3600) / 60;
  int secs = (sec % 3600) % 60;
-System.out.println(+ sec + " seconds = " + hours + " hours, " + minutes + " minutes, " + secs + " seconds");
+System.out.println(+ sec + " seconds = " + hours + " hour, " + minutes + " minutes, " + secs + " seconds");
 
 }
 }
